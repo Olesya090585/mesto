9c64fd055f25523b8df367e886e81c39a9cf1e9c
Olesya090585 <okochman@ya.ru>
2023-03-04 15:33:31 +0300

    fix: исправить замечания после ревью 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,140 +1,150 @@
 // import {initialCardsExport} from './constans.js';
 
 // popup
-const popupEditProfile = document.querySelector('.popup_edit-profile');
-const popupAddPlace = document.querySelector('.popup_add-place');
-const popupZoom = document.querySelector('.popup_zoom');
-const popupImgZoom = document.querySelector('.popup__img_zoom');
-const popupHeadingZoom = document.querySelector('.popup__heading_zoom');
+const popupEditProfile = document.querySelector(".popup_edit-profile");
+const popupAddPlace = document.querySelector(".popup_add-place");
+const popupZoom = document.querySelector(".popup_zoom");
+const popupImgZoom = document.querySelector(".popup__img_zoom");
+const popupHeadingZoom = document.querySelector(".popup__heading_zoom");
 // button popup open
-const popupProfileEditButton = document.querySelector('.profile-info__edit-button');
-const popupCardOpenButton = document.querySelector('.profile__add-button');
+const popupProfileEditButton = document.querySelector(
+  ".profile-info__edit-button"
+);
+const popupCardOpenButton = document.querySelector(".profile__add-button");
 // popup close
-const popupCloseEdit = document.querySelector('.popup__button-close-edit');
-const popupCloseAdd = document.querySelector('.popup__button-close-add');
-const popupCloseZoomImageButton = document.querySelector('.popup__button-close_image-zoom');
+const popupCloseEdit = document.querySelector(".popup__button-close-edit");
+const popupCloseAdd = document.querySelector(".popup__button-close-add");
+const popupCloseZoomImageButton = document.querySelector(
+  ".popup__button-close_image-zoom"
+);
 //elements
-const cardSection = document.querySelector('.elements');
+const cardSection = document.querySelector(".elements");
+const cardTemplate = document.querySelector("#elementTemplate").content; // записываем в переменную cardTeplate шаблон карточки
 //переменная кнопки создания новой карточки
-const formCreateCard = document.querySelector('.popup__content-add');
-// const profilInfoName = document.querySelector('profile-info__title');
-// const profilInfoJob = document.querySelector('profile-info__subtitle');
+const formCreateCard = document.querySelector(".popup__content-add");
+const popupInputTitleAdd = formCreateCard.querySelector('.popup__input-title-add');
+const popupInputImageAdd = formCreateCard.querySelector('.popup__input-image-add');
 //переменная формы при сохранении измененных данных в profile
-const formCard = document.querySelector('.popup__content-edit');
+const formProfile = document.querySelector(".popup__content-edit");
 //находим поля формы в DOM
-const nameInput = formCard.querySelector('.popup__input .popup__input-title-edit');
-const jobInput = formCard.querySelector('.popup__input .popup__input-subtitle-edit');
+const nameInput = formProfile.querySelector(
+  ".popup__input .popup__input-title-edit"
+);
+const jobInput = formProfile.querySelector(
+  ".popup__input .popup__input-subtitle-edit"
+);
 //выберите элементы, куда должны быть вставлены значения полей
-const profilInfoTitle = document.querySelector('.profile-info__title');
-const profileInfoSubtitle = document.querySelector('.profile-info__subtitle');
+const profilInfoTitle = document.querySelector(".profile-info__title");
+const profileInfoSubtitle = document.querySelector(".profile-info__subtitle");
 // функция открытия и закрытия поп-апа
-const togglePopup = function(popup) {
-  popup.classList.toggle('popup_opened');
-}
+const togglePopup = function (popup) {
+  popup.classList.toggle("popup_opened");
+};
 //открываем popup по клику на кнопку
 //popup редактирования profile
-popupProfileEditButton.addEventListener('click', function(){
+popupProfileEditButton.addEventListener("click", function () {
   nameInput.value = profilInfoTitle.textContent;
   jobInput.value = profileInfoSubtitle.textContent;
   togglePopup(popupEditProfile);
 });
 //popup добавления новой карточки
-popupCardOpenButton.addEventListener('click', function(){
+popupCardOpenButton.addEventListener("click", function () {
   togglePopup(popupAddPlace);
 });
 
 // закрываем поп-ап по клику по кнопке
 //popup редактирования profile
-popupCloseEdit.addEventListener('click', closePopupEditProfile);
+popupCloseEdit.addEventListener("click", closePopupEditProfile);
 
-function closePopupEditProfile(){
+function closePopupEditProfile() {
   togglePopup(popupEditProfile);
-};
+}
 //popup добавления новой карточки
-popupCloseAdd.addEventListener('click', closePopupAddElement);
+popupCloseAdd.addEventListener("click", closePopupAddElement);
 
-function closePopupAddElement(){
+function closePopupAddElement() {
   togglePopup(popupAddPlace);
-};
+}
 //popup с увеличенно картинкой
-popupCloseZoomImageButton.addEventListener('click', closePopupimgZoom);
+popupCloseZoomImageButton.addEventListener("click", closePopupimgZoom);
 
-function closePopupimgZoom(){
+function closePopupimgZoom() {
   togglePopup(popupZoom);
-};
+}
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+function handleFormProfileSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // Получите значение полей jobInput и nameInput из свойства value                                               // О том, как это делать, расскажем позже.
-const nameInputValue = nameInput.value;
-const jobInputValue = jobInput.value;
-//вставьте новые значения с помощью textContent
-profilInfoTitle.textContent = nameInputValue;
-profileInfoSubtitle.textContent = jobInputValue;
-closePopupEditProfile();
+  const nameInputValue = nameInput.value;
+  const jobInputValue = jobInput.value;
+  //вставьте новые значения с помощью textContent
+  profilInfoTitle.textContent = nameInputValue;
+  profileInfoSubtitle.textContent = jobInputValue;
+  closePopupEditProfile();
 }
 //прикрепляем обработчик к форме:
 //он будет следить за событием “submit” - «отправка»
-formCard.addEventListener('submit', handleFormSubmit);
+formProfile.addEventListener("submit", handleFormProfileSubmit);
 
-//добавляем элементы в секцию elements
+//создаем новую карточку
 const createCard = (card) => {
-  const cardNew = document.querySelector('#elementTemplate').content.cloneNode(true);
-  const cardName = cardNew.querySelector('.element__text');
+  const cardNew = cardTemplate.cloneNode(true);
+  const cardName = cardNew.querySelector(".element__text");
   cardName.textContent = card.name;
-  const cardImage = cardNew.querySelector('.element__img');
-  cardImage.setAttribute('src', card.link);
-  cardImage.setAttribute('alt', card.alt);
-  // cardSection.prepend(cardNew);
-  renderCard(cardNew, cardSection);
-  const cardDeleteButton = document.querySelector('.element__delete_button');
-  cardDeleteButton.addEventListener('click', deleteCard);
-  const cardLikeButton = document.querySelector('.element__like_button');
-  cardLikeButton.addEventListener('click', clickLikeAсtive);
-  const cardClickImageZoom = document.querySelector('.element__img_zoom');
-  cardClickImageZoom.addEventListener('click', createPopupImage);
+  const cardImage = cardNew.querySelector(".element__img");
+  cardImage.setAttribute("src", card.link);
+  cardImage.setAttribute("alt", card.alt);
+  const cardDeleteButton = cardNew.querySelector(".element__delete_button"); // ищем кнопку не по всему документу, а внутри cardNew - это копия шаблона со всем содержимым карточки - нужно обращатсья к нему
+  cardDeleteButton.addEventListener("click", deleteCard);
+  const cardLikeButton = cardNew.querySelector(".element__like_button"); // ищем кнопку не по всему документу, а внутри cardNew - это копия шаблона со всем содержимым карточки - нужно обращатсья к нему
+  cardLikeButton.addEventListener("click", clickLikeAсtive);
+  const cardClickImageZoom = cardNew.querySelector(".element__img_zoom"); // ищем кнопку не по всему документу, а внутри cardNew - это копия шаблона со всем содержимым карточки - нужно обращатсья к нему
+  cardClickImageZoom.addEventListener("click", createPopupImage);
+  return cardNew;
 };
+////добавляем карточку на страницу
+const addCard = (card) => {
+  cardSection.prepend(createCard(card));
+};
+//переворачиваем массив и сохранем в переменную
 const containerCard = initialCards.reverse();
-containerCard.forEach(createCard);
-
-function renderCard(card, container) {
-  container.prepend(card);
-}
-
+//проходим по массиву функцией addCard
+containerCard.forEach(addCard);
 //создаем новую карточку по событию submit
-formCreateCard.addEventListener('submit', handleFormElementSubmit);
+formCreateCard.addEventListener("submit", handleFormElementSubmit);
 //функция создания новой карточки из данных в popup
-function handleFormElementSubmit(event){
+function handleFormElementSubmit(event) {
   event.preventDefault();
-  const formCard = event.target;
-  const nameElement = formCard.querySelector('.popup__input-title-add').value;
-  const imageElement = formCard.querySelector('.popup__input-image-add').value;
+  const formProfile = event.target;
+  const nameElement = formProfile.querySelector(".popup__input-title-add").value;
+  const imageElement = formProfile.querySelector(".popup__input-image-add").value;
   const anotherElement = {
     name: nameElement,
     alt: nameElement,
-    link: imageElement
-    }
-    createCard(anotherElement);
-    closePopupAddElement();
-    event.target.reset();
- }
+    link: imageElement,
+  };
+  addCard(createCard(anotherElement));
+  closePopupAddElement();
+  // event.target.reset();
+  popupInputTitleAdd.value = '';
+  popupInputImageAdd.value = '';
+}
 
 //функция удаления элемента
-function deleteCard(event){
-  event.target.closest('.element').remove();
-
+function deleteCard(event) {
+  event.target.closest(".element").remove();
 }
- // like
-function clickLikeAсtive(event){
-  event.target.classList.toggle('element__like_active');
-};
+// like
+function clickLikeAсtive(event) {
+  event.target.classList.toggle("element__like_active");
+}
 //передаем данные элемента в popup с картинкой и открываем его
-function createPopupImage (event){
+function createPopupImage(event) {
   popupImgZoom.src = event.target.src;
   popupImgZoom.alt = event.target.alt;
   popupHeadingZoom.textContent = popupImgZoom.alt;
   togglePopup(popupZoom);
-};
+}
