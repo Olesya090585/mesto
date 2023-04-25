8d7ac8569e4d969ad4291c625eb23ed11f6021f3
Olesya090585 <okochman@ya.ru>
2023-04-25 10:28:49 +0300

    project8-r2

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import { Section } from "../components/Section.js";
 // import { Popup } from "./Popup.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
-import { UserInfo } from "../components/UzerInfo.js";
+import { UserInfo } from "../components/UserInfo.js";
 import {
   popupProfileEditButton,
   popupCardOpenButton,
